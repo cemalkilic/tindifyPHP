b7876ebd6ef7947e5252e4472ad8158b185632e7
Cemal Kılıç <cemalkilic96@gmail.com>
2020-04-11 01:52:42 +0300

    Doctrine is here!

--- config/bundles.php
+++ config/bundles.php
@@ -3,4 +3,5 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
 ];
