6936b4348cb7947802a8f202d1a6045d4fa1b0bc
Cemal Kılıç <cemalkilic96@gmail.com>
2020-04-11 15:34:25 +0300

    POST /song/recommendations :: Song recmnds based on seed songs

--- src/Controller/SongController.php
+++ src/Controller/SongController.php
@@ -46,4 +46,37 @@ class SongController extends AbstractController {
         return JsonResponse::create($res);
     }
 
+    public function getRecommendations(Request $request) {
+        $res = [
+            "success" => false,
+            "message" => "Get recommended songs based on the given songs!",
+            "content" => []
+        ];
+
+        $seedSongs = $request->request->get('seedSongs', null);
+        if (empty($seedSongs)) {
+            $res['message'] = "No seed songs givens!";
+            return JsonResponse::create($res, 400);
+        }
+
+        if (!is_array($seedSongs)) {
+            $res['message'] = "Seed songs should be given as an array of songs IDs!";
+            return JsonResponse::create($res, 400);
+        }
+
+        $options = [
+            "limit"       => 20, // number of returned songs
+            "seed_tracks" => $seedSongs
+        ];
+
+        try {
+            $res['content'] = $this->api->getRecommendations($options);
+            $res['success'] = true;
+        } catch (SpotifyWebAPIException $e) {
+            $res['message'] = $e->getReason() ?? $e->getMessage();
+        }
+
+        return JsonResponse::create($res);
+    }
+
 }
