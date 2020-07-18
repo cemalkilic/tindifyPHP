45570b9343fc5fa8c353cf26ea704c4052680b7a
Cemal Kılıç <cemalkilic96@gmail.com>
2020-07-18 16:42:39 +0300

    IndexController :: Show homepage in any case

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -10,16 +10,7 @@ use Symfony\Component\HttpFoundation\Response;
 class IndexController extends AbstractController {
 
     public function index() {
-
-        // If user is authenticated, show the app homepage
-        // Show landing page otherwise.
-        if ($this->isGranted('IS_AUTHENTICATED_FULLY')) {
-            return $this->render("base.html.twig");
-        } else {
-            // TODO landing template here
-            $content = '<a href="/auth"> Click here to login</a>';
-            return new Response($content);
-        }
+        return $this->render("base.html.twig");
     }
 
 }
